291ff371bf9701306134522cb57d29846873101f
Jeisson <jeissoncar97@gmail.com>
2024-09-26 21:48:47 -0500

    basic

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,12 +4,7 @@ const About = () => {
 	return (
 		<section className="about">
 			<h2>ABOUT ME</h2>
-			<iframe
-				src="https://www.youtube.com/embed/OmRotR0EdhI?si=UIdffN7X4-MbuFQJ"
-				title="YouTube video player"
-				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-				allowfullscreen
-			></iframe>
+			
 		</section>
 	);
 };
