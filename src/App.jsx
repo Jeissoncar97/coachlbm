075689db60229dab318e5602641791eb6d268266
Jeisson Cardenas <jeissoncar97@gmail.com>
2024-10-01 16:57:51 -0500

    add section Faqs

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./components/Header"
 import About from "./components/About"
 import Offer from "./components/Offer"
 import Plans from "./components/Plans"
-import FAQs from "./components/FAQs"
+import FAQs from "./components/Faqs"
 import PersonalizedReport from '../src/components/PersonalizedReports'
 import './index.css'
 
